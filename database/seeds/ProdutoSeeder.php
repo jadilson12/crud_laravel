d68aedb00ca2a25b4318635f38ac30c227c159a2
Jadilson Guedes <jadilson12@gmail.com>
2019-04-11 23:01:37 -0300

    seeds: adiciona dados demo para  produto

--- database/seeds/ProdutoSeeder.php
+++ database/seeds/ProdutoSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class ProdutoSeeder extends Seeder
 {
@@ -11,6 +13,30 @@ class ProdutoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // valores demos
+
+        DB::table('produtos')->insert([
+            [
+                'nome' => 'Notebook',
+                'descricao' => 'Completo com HD 500 GB',
+                'quantidade' => 3,
+                'preco' => 2.5,
+                'categoria_id' => 1,
+                'created_at' => date("Y-m-d H:m:s"),
+                'updated_at' => date("Y-m-d H:m:s"),
+            ],
+        ]);
+
+        DB::table('produtos')->insert([
+            [
+                'nome' => 'Teclado',
+                'descricao' => 'Para nootebook',
+                'quantidade' => 1,
+                'preco' => 4.5,
+                'categoria_id' => 1,
+                'created_at' => date("Y-m-d H:m:s"),
+                'updated_at' => date("Y-m-d H:m:s"),
+            ],
+        ]);
     }
 }
