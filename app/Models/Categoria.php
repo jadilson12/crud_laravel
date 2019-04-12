681715c65f2e93f9325906c69fbcf37c938e678f
Jadilson Guedes <jadilson12@gmail.com>
2019-04-11 22:58:30 -0300

    models: adiciona atributos na model Categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    // fillable define quais os campos que podem ser inseridos pelo usuário do sistema no Banco
+
+    protected $fillable = ['nome']    ;
+
+    // guarded protege os campos de inserções impede que alguém insira dados em alguns campos da nossa tabela.
+
+    protected $guarded = [ 'created_at', 'update_at'];
 }
