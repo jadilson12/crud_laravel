11955402dbb9cf127746c25e6b53f4cecf3384cb
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 09:29:22 -0300

    view: adicionado layout app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+
+    <!-- custom -->
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+
+    <title> Cadastro de produtos </title>
+{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
+
+</head>
+<body>
+<div class="container">
+    @component('components.navbar',[ "current" => $current])
+    @endcomponent
+    <main role="main">
+        @hasSection('body')
+            @yield('body')
+        @endif
+    </main>
+</div>
+
+<!-- Bootstrap -->
+<script src="{{ asset('js/app.js')}}" type="text/javascript" ></script>
+
+</body>
+</html>
