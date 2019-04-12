73b17bf35f542d4acdb91e68834a30b6435f79f0
Jadilson Guedes <jadilson12@gmail.com>
2019-04-12 09:28:43 -0300

    view: adicionado navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav mr-auto">
+            <li @if ($current =="home") class="nav-item active" @else class="nav-item" @endif >
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            <li @if ($current =="produtos") class="nav-item active" @else class="nav-item" @endif >
+                <a class="nav-link" href="{{route('produtos.index')}}">Produto</a>
+            </li>
+            <li @if ($current =="categoria") class="nav-item active" @else class="nav-item" @endif >
+                <a class="nav-link" href="{{route('categorias.index')}}">Categoria</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
