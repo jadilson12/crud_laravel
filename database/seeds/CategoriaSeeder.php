40210db866067f9c329787906c1247a3e41e784c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-11 23:01:13 -0300

    seeds: adiciona dados demo para  categoria

--- database/seeds/CategoriaSeeder.php
+++ database/seeds/CategoriaSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CategoriaSeeder extends Seeder
 {
@@ -11,6 +13,21 @@ class CategoriaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Valores demos
+
+        DB::table('categorias')->insert([
+            [
+                'nome' => 'Informatica',
+                'created_at' => date("Y-m-d H:m:s"),
+                'updated_at' => date("Y-m-d H:m:s"),
+            ],
+        ]);
+        DB::table('categorias')->insert([
+            [
+                'nome' => 'Carros',
+                'created_at' => date("Y-m-d H:m:s"),
+                'updated_at' => date("Y-m-d H:m:s"),
+            ],
+        ]);
     }
 }
