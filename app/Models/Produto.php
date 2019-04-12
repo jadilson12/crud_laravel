12aa0cce52c11d612b6decafa25a13fedc7f8d3d
Jadilson Guedes <jadilson12@gmail.com>
2019-04-11 22:58:51 -0300

    models: adiciona atributos na model Produto

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    // fillable define quais os campos que podem ser inseridos pelo usuário do sistema no Banco
+
+    protected $fillable = ['nome','descricao','quantidade','preco'];
+
+    // guarded protege os campos de inserções impede que alguém insira dados em alguns campos da nossa tabela.
+
+    protected $guarded = ['created_at', 'update_at'];
 }
