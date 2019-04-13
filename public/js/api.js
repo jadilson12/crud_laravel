b758b43ade1060adc31a87028320ecb762185280
Jadilson Guedes <jadilson12@gmail.com>
2019-04-13 20:51:08 -0300

    js/api: adicona opção para carregar categoria

--- public/js/api.js
+++ public/js/api.js
@@ -3,3 +3,18 @@ $.ajaxSetup({
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
 });
+
+function carregarCategorias() {
+    $.getJSON('/api/categorias', function(data) {
+        for(i=0;i<data.length;i++) {
+            opcao = '<option value ="' + data[i].id + '">' +
+                data[i].nome + '</option>';
+            $('#categoriaProduto').append(opcao);
+        }
+    });
+}
+
+
+$(function () {
+    carregarCategorias();
+});
