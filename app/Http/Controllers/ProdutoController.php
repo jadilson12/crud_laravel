5597663ab545e6202c315d1b8c3578d78587c753
Jadilson Guedes <jadilson12@gmail.com>
2019-04-13 20:37:36 -0300

    controller: atualizando o update ProdutoController para uso API

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -92,19 +92,18 @@ class ProdutoController extends Controller
      */
     public function update(Request $request, $id)
     {
+
         $produto = Produto::find($id);
-
-        if(isset($produto)){
+        if (isset($prod)) {
             $produto->nome = $request->input('nome');
-            $produto->descricao = $request->input('descricao');
-            $produto->quantidade = $request->input('quantidade');
             $produto->preco = $request->input('preco');
-            $produto->categoria_id = $request->input('categorias');
+            $produto->estoque = $request->input('estoque');
+            $produto->categoria_id = $request->input('categoria_id');
             $produto->save();
+            return json_encode($produto);
         }
-        return response('Produto não encontado', 404);
+        return response('Produto não encontrado', 404);
     }
-
 
     /**
      * Remove the specified resource from storage.
