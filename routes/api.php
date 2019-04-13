0b1884c5573f48b724a4676408b7132f35724bd2
Jadilson Guedes <jadilson12@gmail.com>
2019-04-13 20:13:21 -0300

    route api: adicionado rota para usar produto

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/categorias', 'CategoriaController@indexJson');
+
+Route::resource('/produtos', 'ProdutoController');
