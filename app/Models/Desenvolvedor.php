d275015eb808fdb62172af8cdebd4dd6c0f9626c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-14 15:09:58 -0300

    model: adicionando anotação para desenvolver

--- app/Models/Desenvolvedor.php
+++ app/Models/Desenvolvedor.php
@@ -9,4 +9,10 @@ class Desenvolvedor extends Model
     // Isso é obrigaorio porque foi renomeado a tabela
 
     protected $table = 'desenvolvedores';
+
+    // Desenvolvedor pertecem vários projetos
+
+    function projetos(){
+        return $this->belongsToMany('App\Models\Densevolvedor','alocacoes');
+    }
 }
