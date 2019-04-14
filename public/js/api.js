8cf1da800a77cb319a8abd6ff254356448c96d37
Jadilson Guedes <jadilson12@gmail.com>
2019-04-13 21:07:02 -0300

    js/api: adicona opção para realizar o DELETE do produto

--- public/js/api.js
+++ public/js/api.js
@@ -97,6 +97,26 @@ function montarLinha(p) {
     return linha;
 }
 
+function remover(id) {
+    $.ajax({
+        type: "DELETE",
+        url: "/api/produtos/" + id,
+        context: this,
+        success: function() {
+            console.log('Apagou OK');
+            linhas = $("#tabelaProdutos>tbody>tr");
+            e = linhas.filter( function(i, elemento) {
+                return elemento.cells[0].textContent == id;
+            });
+            if (e)
+                e.remove();
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
 function carregarProdutos(){
     $.getJSON('/api/produtos', function (produtos) {
         for (i = 0; i < produtos.length; i++) {
