dde52ef21ae1969a76dbbc0f981736afed2b1a6c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-15 17:12:56 -0300

    view: arrumando poucas coisas

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -2,11 +2,10 @@
 
 @section('body')
     <div class="card border">
-{{--        <div class="card text-center">--}}
             <div class="card-header text-center">Tabela de Clientes
             </div>
             <div class="card-body">
-                <h5 class="card-title">Exibindo {{$clientes->count()}} clientes de {{$clientes->total()}}
+                <h5 class="card-title text-center">Exibindo {{$clientes->count()}} clientes de {{$clientes->total()}}
                     ( {{$clientes->firstItem()}} a {{$clientes->lastItem()}} ) </h5>
 
                 <table class="table table-hover">
@@ -30,10 +29,9 @@
                     </tbody>
                 </table>
             </div>
-            <div class="card-footer">
+            <div class="card-footer float-md-right">
                 {{ $clientes->links() }}
             </div>
-{{--        </div>--}}
     </div>
 
 <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
